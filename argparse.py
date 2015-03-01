43bbdec6cb9308ff7cd076508a4c46a072ba1b6f
Kyle Suarez <ksuarz@gmail.com>
2015-02-28 22:25:56 -0500

    Use double quotes instead of single quotes

--- argparse.py
+++ argparse.py
@@ -1,31 +1,31 @@
 import argparse
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Set up command line options
     parser = argparse.ArgumentParser(
-            prog='',
-            description='',
-            epilog='')
+            prog="",
+            description="",
+            epilog="")
     parser.add_argument(
-            'positional-argument',
-            nargs='+',
-            help='')
+            "positional-argument",
+            nargs="+",
+            help="")
     parser.add_argument(
-            '-b',
-            '--boolean-option',
-            action='store_true',
-            help='')
+            "-b",
+            "--boolean-option",
+            action="store_true",
+            help="")
     parser.add_argument(
-            '-i',
-            '--integer-option',
-            metavar='INT',
+            "-i",
+            "--integer-option",
+            metavar="INT",
             default=0,
-            dest='var_name_to_store_in',
+            dest="var_name_to_store_in",
             type=int,
-            help='')
+            help="")
     parser.add_argument(
-            '--long-option',
-            metavar='OPTION',
-            default='',
-            help='')
+            "--long-option",
+            metavar="OPTION",
+            default="",
+            help="")
